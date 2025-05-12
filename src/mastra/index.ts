301ad3672e70d6d927dc9885d909fd7350ba1b3a
Kevin Traver <kevintraver@kevintraver.com>
2025-05-12 17:07:24 -0600

    Revert "build: add mastra deployer vercel"
    This reverts commit 261690c8b9b48f655f596bdeca0116c2972bd6ea.

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,18 +1,12 @@
-import { Mastra } from '@mastra/core/mastra'
-import { VercelDeployer } from '@mastra/deployer-vercel'
-import { createLogger } from '@mastra/core/logger'
+import { Mastra } from "@mastra/core/mastra";
+import { createLogger } from "@mastra/core/logger";
 
-import { mastraAgent } from './agents'
+import { mastraAgent } from "./agents";
 
 export const mastra = new Mastra({
   agents: { mastraAgent },
   logger: createLogger({
-    name: 'Mastra',
-    level: 'info'
+    name: "Mastra",
+    level: "info",
   }),
-  deployer: new VercelDeployer({
-    teamSlug: 'chameleon-invoice',
-    projectName: 'mastra',
-    token: process.env.VERCEL_TOKEN as string
-  })
-})
+});
