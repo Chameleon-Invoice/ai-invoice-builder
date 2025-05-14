9e7f524bfa4e1f7024bb6562a6d4426d23085ac3
Branden Silva <branden@ambition.studio>
2025-05-14 17:26:58 -0600

    build(config): style and config adjustments

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    remotePatterns: [new URL('https://placehold.co/150x150?text=logo')],
+    dangerouslyAllowSVG: true
+  }
+}
 
-export default nextConfig;
+export default nextConfig
