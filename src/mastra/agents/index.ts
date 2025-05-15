8ce7591727d6f0dfaaa3e1e3fac5c614787f83ec
Kevin Traver <kevintraver@kevintraver.com>
2025-05-14 19:13:22 -0600

    feat: add resend mcp

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -9,6 +9,9 @@ dotenv.config()
 const smitheryApiKey = process.env.SMITHERY_API_KEY || ''
 const exaApiKey = process.env.EXA_API_KEY || ''
 const exaConfig = `"{\\\"exaApiKey\\\":\\\"${exaApiKey}\\\"}"`
+const resendProfile = process.env.RESEND_PROFILE || ''
+const senderEmail = process.env.RESEND_SENDER_EMAIL_ADDRESS || ''
+const replyToEmail = process.env.RESEND_REPLY_TO_EMAIL_ADDRESS || ''
 
 // Configure the Smithery-hosted Exa MCP server
 const mcp = new MCPClient({
@@ -25,6 +28,23 @@ const mcp = new MCPClient({
         '--config',
         exaConfig
       ]
+    },
+    'mcp-send-email': {
+      command: 'npx',
+      args: [
+        '-y',
+        '@smithery/cli@latest',
+        'run',
+        '@resend/mcp-send-email',
+        '--key',
+        smitheryApiKey,
+        '--profile',
+        resendProfile,
+        '--sender',
+        senderEmail,
+        '--reply-to',
+        replyToEmail
+      ]
     }
   }
 })
@@ -33,6 +53,22 @@ export const mastraAgent = new Agent({
   name: 'Invoice Agent',
   instructions: `
       You are a helpful assistant, which helps create invoices.
+
+      Use the mcp-send-email tool to send emails.
+
+      Before sending, always format the email in markdown like this:
+
+      to: <recipient email>
+      subject: <subject line>
+      text: <plain text body>
+      html: <optional, HTML body>
+
+      Only include fields the user provides or confirms. Do not invent or autofill addresses.
+
+      Do not ask to cc or bcc anyone
+      Do not preview the email. Do not ask to confirm. Do not schedule the email, send it out immediately.
+
+      
 `,
   model: openai('gpt-4o'),
   tools: await mcp.getTools()
