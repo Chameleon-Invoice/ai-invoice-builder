a7cf64a60dfb7d082b750367daa02fbc9fd334d0
Kevin Traver <kevintraver@kevintraver.com>
2025-05-14 21:58:55 -0600

    refactor: extract tools to folder

--- src/mastra/agents/index.ts
+++ src/mastra/agents/index.ts
@@ -1,53 +1,6 @@
 import { openai } from '@ai-sdk/openai'
 import { Agent } from '@mastra/core/agent'
-import { MCPClient } from '@mastra/mcp'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-// Prepare the Smithery API key and Exa API key config for CLI
-const smitheryApiKey = process.env.SMITHERY_API_KEY || ''
-const exaApiKey = process.env.EXA_API_KEY || ''
-const exaConfig = `"{\\\"exaApiKey\\\":\\\"${exaApiKey}\\\"}"`
-const resendProfile = process.env.RESEND_PROFILE || ''
-const senderEmail = process.env.RESEND_SENDER_EMAIL_ADDRESS || ''
-const replyToEmail = process.env.RESEND_REPLY_TO_EMAIL_ADDRESS || ''
-
-// Configure the Smithery-hosted Exa MCP server
-const mcp = new MCPClient({
-  servers: {
-    exa: {
-      command: 'npx',
-      args: [
-        '-y',
-        '@smithery/cli@latest',
-        'run',
-        'exa',
-        '--key',
-        smitheryApiKey,
-        '--config',
-        exaConfig
-      ]
-    },
-    'mcp-send-email': {
-      command: 'npx',
-      args: [
-        '-y',
-        '@smithery/cli@latest',
-        'run',
-        '@resend/mcp-send-email',
-        '--key',
-        smitheryApiKey,
-        '--profile',
-        resendProfile,
-        '--sender',
-        senderEmail,
-        '--reply-to',
-        replyToEmail
-      ]
-    }
-  }
-})
+import { mcpTools } from '../tools'
 
 export const mastraAgent = new Agent({
   name: 'Invoice Agent',
@@ -71,5 +24,5 @@ export const mastraAgent = new Agent({
       
 `,
   model: openai('gpt-4o'),
-  tools: await mcp.getTools()
+  tools: mcpTools
 })
