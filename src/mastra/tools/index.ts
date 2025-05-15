14a245db5b2e412cdefa7fdfdd4d9c4b722171c8
Kevin Traver <kevintraver@kevintraver.com>
2025-05-14 21:58:55 -0600

    refactor: extract tools to folder

---
+++ src/mastra/tools/index.ts
@@ -0,0 +1,51 @@
+import { MCPClient } from '@mastra/mcp'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// Prepare the Smithery API key and Exa API key config for CLI
+const smitheryApiKey = process.env.SMITHERY_API_KEY || ''
+const exaApiKey = process.env.EXA_API_KEY || ''
+const exaConfig = `"{\\\"exaApiKey\\\":\\\"${exaApiKey}\\\"}"`
+const resendProfile = process.env.RESEND_PROFILE || ''
+const senderEmail = process.env.RESEND_SENDER_EMAIL_ADDRESS || ''
+const replyToEmail = process.env.RESEND_REPLY_TO_EMAIL_ADDRESS || ''
+
+// Configure the Smithery-hosted Exa MCP server
+export const mcp = new MCPClient({
+  servers: {
+    exa: {
+      command: 'npx',
+      args: [
+        '-y',
+        '@smithery/cli@latest',
+        'run',
+        'exa',
+        '--key',
+        smitheryApiKey,
+        '--config',
+        exaConfig
+      ]
+    },
+    'mcp-send-email': {
+      command: 'npx',
+      args: [
+        '-y',
+        '@smithery/cli@latest',
+        'run',
+        '@resend/mcp-send-email',
+        '--key',
+        smitheryApiKey,
+        '--profile',
+        resendProfile,
+        '--sender',
+        senderEmail,
+        '--reply-to',
+        replyToEmail
+      ]
+    }
+  }
+})
+
+// Export MCP tools
+export const mcpTools = await mcp.getTools()
