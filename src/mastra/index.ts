ee6c73fe70cb5ca5fb10136505e9a0b170dfb81b
Kevin Traver <kevintraver@kevintraver.com>
2025-05-14 21:32:33 -0600

    feat: start company url workflow

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -2,9 +2,13 @@ import { Mastra } from "@mastra/core/mastra";
 import { createLogger } from "@mastra/core/logger";
 
 import { mastraAgent } from "./agents";
+import { companyUrlWorkflow } from "./workflows";
 
 export const mastra = new Mastra({
   agents: { mastraAgent },
+  vnext_workflows: {
+    companyUrlWorkflow,
+  },
   logger: createLogger({
     name: "Mastra",
     level: "info",
