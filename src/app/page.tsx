381f622ad430bd1db9ac642b293c3f1cbba403a3
Branden Silva <branden@ambition.studio>
2025-05-16 08:30:11 -0600

    feat: add favicon and meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,18 @@
-'use client'
 
-import React, { useEffect } from 'react'
-import { Undo2 } from 'lucide-react'
-import '@copilotkit/react-ui/styles.css'
-import './globals.css'
-import { CopilotKit } from '@copilotkit/react-core'
-import { InvoicePreview } from '@/components/invoice-preview'
-import { InvoiceIntro } from '@/components/invoice-intro'
-import { InvoiceChat } from '@/components/invoice-chat'
-import { useSelector } from '@xstate/store/react'
-import { invoiceStore } from '@/store/invoice-store'
-import { Button } from '@/components/ui/button'
+import { ChameleonInvoice } from '@/components/chameleon-invoice'
+import type { Metadata } from 'next'
 
-const ChameleonInvoice: React.FC = () => {
-  const context = useSelector(invoiceStore, (state) => state.context)
-
-  useEffect(() => {
-    document.documentElement.style.setProperty('--primary', context.customization.primaryColor)
-  }, [context.customization.primaryColor])
-  return (
-    <>
-      {context.hasStarted ? (
-        <InvoiceIntro />
-      ) : (
-        <div className='grid grid-cols-1 md:grid-cols-12 w-full gap-6 p-4 md:p-0'>
-          <div className='col-span-1 md:col-span-8'>
-            <InvoicePreview />
-          </div>
-          <div className='col-span-1 md:col-span-4'>
-            <CopilotKit
-              runtimeUrl='/api/copilotkit'
-              agent='mastraAgent'
-            >
-              <InvoiceChat />
-            </CopilotKit>
-            <div className='flex flex-col gap-4 mx-auto w-full md:w-8/10 items-stretch px-4 md:px-0'>
-              <Button
-                onClick={() => invoiceStore.send({ type: 'reset' })}
-                variant='link' className='text-red-600'>
-                <Undo2 className="mr-1 size-5" />
-                Reset
-              </Button>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  )
+export const metadata: Metadata = {
+  title: 'Chamelon Invoice',
+  description: 'Invoicing made simple. Done before you notice.',
+  icons: {
+    icon: '/favicon.ico',
+  }
 }
 
-export default ChameleonInvoice
+export default function Page() {
+
+  return (
+    <ChameleonInvoice />
+  )
+}
